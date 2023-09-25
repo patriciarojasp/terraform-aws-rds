6814b5170ca09514ed7e1daad7072edb30c6a0a2
Patricia <patricia.rojasperez@hashicorp.com>
2023-09-25 10:03:42 -0500

    adding string type to variables

--- variables.tf
+++ variables.tf
@@ -1,12 +1,15 @@
 variable "db_name" {
   description = "Unique name to assign to RDS instance"
+  type = string
 }
 
 variable "db_username" {
   description = "RDS root username"
+  type = string
 }
 
 variable "db_password" {
   description = "RDS root user password"
+  type = string
   sensitive   = true
 }
